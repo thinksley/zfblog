a8673a934d63b9a707799f113a197462d3c94259
李少祥 <939480781@qq.com>
2016-01-25 16:38:06 +0800

    0

--- routes/article.js
+++ routes/article.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var markdown = require('markdown').markdown;
 
 router.get('/add', function(req, res) {
 
@@ -16,8 +16,15 @@ router.post('/add', function (req, res) {
     }
     //req.flash('success', 'success!');  //放置成功信息
     res.redirect('/');//发表文章成功后返回主页
-  });
+  })
+});
 
+router.get('/detail/:_id', function (req, res) {
+  console.log(req.params._id);
+  Model('Article').findOne({_id:req.params._id},function(err,article){
+    article.content = markdown.toHTML(article.content);
+    res.render('article/detail',{title:'查看文章',article:article});
+  });
 });
 
 module.exports = router;
